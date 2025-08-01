9ca54b6bc21f1f0ab0479f3457cc51c39340b7b2
BuddyBot Developer <test@example.com>
2025-08-01 11:41:29 +0000

    auto-commit for 5dba3c7b-6d74-4b66-8bc2-1fea9377ec7d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -950,6 +950,7 @@ const App = () => {
         user={user} 
         onOpenProfile={() => setIsProfileSetupOpen(true)}
         onOpenSettings={() => setIsParentalControlsOpen(true)}
+        onLogout={handleLogout}
       />
       <div className="flex-1 overflow-auto">
         <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 p-4">
