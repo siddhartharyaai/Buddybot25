f37b53a06e51a923fe3ae5fce052db7d6a254324
BuddyBot Developer <test@example.com>
2025-08-01 11:39:13 +0000

    auto-commit for d25fc46a-c7ce-48ab-90e3-33b51574b709

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -876,6 +876,7 @@ const App = () => {
         user={user} 
         onOpenProfile={() => setIsProfileSetupOpen(true)}
         onOpenSettings={() => setIsParentalControlsOpen(true)}
+        onLogout={handleLogout}
       />
       {/* Mobile Greeting Button */}
       {needsGestureForAudio && (
