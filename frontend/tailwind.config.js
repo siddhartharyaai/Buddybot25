e7e3f3787497cdd8b44cb713598e2c46af6ad0dd
BuddyBot Developer <test@example.com>
2025-08-02 14:13:35 +0000

    auto-commit for 918808c4-2967-45d0-86e8-697a9024b028

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px', // Extra small devices (very small phones)
+      },
+    },
   },
   plugins: [],
 };
